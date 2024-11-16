bbe21670a1d5c4701ecf36ae6f781c7e68a2f716
Adityadhopte <adityadhopte17@gmail.com>
2024-11-17 01:55:21 +0530

    first commit, Part 1 complete, setup complete

---
+++ presentation/src/main/java/com/example/ecommerceapp/ShopperApp.kt
@@ -0,0 +1,22 @@
+package com.example.ecommerceapp
+
+import android.app.Application
+import com.example.data.di.dataModule
+import com.example.domain.di.domainModule
+import com.example.ecommerceapp.di.presentationModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class ShopperApp: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin{
+            androidContext(this@ShopperApp)
+            modules(listOf(
+                presentationModule,
+                dataModule,
+                domainModule))
+    }
+        }
+}
---
+++ presentation/src/main/java/com/example/ecommerceapp/di/ViewModelModule.kt
@@ -0,0 +1,11 @@
+package com.example.ecommerceapp.di
+
+import com.example.ecommerceapp.ui.feature.home.HomeViewModel
+import org.koin.core.module.dsl.viewModel
+import org.koin.dsl.module
+
+val viewModelModule = module {
+    viewModel {
+        HomeViewModel(get())
+    }
+}
